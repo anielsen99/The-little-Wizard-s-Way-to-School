b76d9b58038f5d92d331863e534e202b6b81fee3
SteffiKottmann <steffi-kottmann@gmx.de>
2026-08-21 16:27:32 +0200

    added tree-resin and deleted old mushroom

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ class GameScene extends Phaser.Scene {
       // Assets laden
       this.load.image('ground', 'https://labs.phaser.io/assets/sprites/platform.png');
 
-      this.load.image('mushroom', 'media/mushroom_2.png');
+      this.load.image('mushroom', 'media/mushroom.png');
+      this.load.image('tree-resin', 'media/tree-resin.png');
     }
   
     create() {
@@ -29,6 +30,7 @@ class GameScene extends Phaser.Scene {
       this.physics.add.collider(this.wizard, platforms);
 
       const item = this.add.image(100, 300, 'mushroom');
+      const item = this.add.image(500, 150, 'tree-resin');
       //item.setScale(0.05);
   
       // 3. Tastatursteuerung
